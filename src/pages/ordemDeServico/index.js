b70354e03d1b97809ea5da1d19be505cf1819ee8
0ld3nburg <ceduburg@gmail.com>
2023-11-08 21:04:48 -0300

    formatando datas

--- src/pages/ordemDeServico/index.js
+++ src/pages/ordemDeServico/index.js
@@ -13,20 +13,32 @@ let cliente = document.getElementById('cliente');
 button.addEventListener('click', async (event) => {
   console.log(descricao.value, statusOrdem.value, inicio.value, conclusao.value, modelo.value, marca.value, valor.value, cliente.value);
 
-  // let dataCriacao = new Date.now();
-  // let hoje = new Date(dataCriacao)
-  // hoje.toDateString();
-
   let clienteNumero = Number(cliente.value);
 
-  console.log(clienteNumero);
+  function formatarDataParaBackend(data) {
+    let ano = data.getFullYear();
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
+    let dia = String(data.getDate()).padStart(2, '0');
+    let horas = String(data.getHours()).padStart(2, '0');
+    let minutos = String(data.getMinutes()).padStart(2, '0');
+    let segundos = String(data.getSeconds()).padStart(2, '0');
+
+    return `${ano}-${mes}-${dia}T${horas}:${minutos}:${segundos}`;
+  }
+
+  let inicioDate = new Date(inicio.value);
+  let conclusaoDate = new Date(conclusao.value);
+
+  let inicioFormatado = formatarDataParaBackend(inicioDate);
+  let conclusaoFormatada = formatarDataParaBackend(conclusaoDate);
+
 
   let payload = {
-    dataCriacao: "2023-11-06T12:00:00",
+    dataCriacao: new Date().toISOString(),
     descricaoServico: descricao.value,
     statusOrdemServico: statusOrdem.value,
-    dataInicioServico: inicio.value,
-    dataConclusaoServico: conclusao.value,
+    dataInicioServico: inicioFormatado,
+    dataConclusaoServico: conclusaoFormatada,
     bicicletaModelo : modelo.value,
     bicicletaMarca: marca.value,
     valor: valor.value,
